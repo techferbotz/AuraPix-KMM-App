0908a7cd2819f471d6b5b84804ee41eed40cb988
vasikaran <vasikaran8702@gmail.com>
2026-09-21 15:55:51 +0530

    app version to 1.0.0

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "com.ferbotz.aurapix"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.0"
     }
     packaging {
         resources {
